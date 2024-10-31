78bb3397e9749ab5a18f8c66c31e94c91ee44bc2
noumanhafeez <noumanhafeez095@gmail.com>
2024-10-31 19:53:54 +0500

    add github links

--- lib/data.ts
+++ lib/data.ts
@@ -27,21 +27,21 @@ export const projectsData: ProjectInfo[] = [
         description: "A React Native coffee shop app with smooth navigation and Redux for state management, and Immer for immutability.",
         tags: ["React Native", "React Navigation", "Redux", "Immer",],
         imageUrl: "/CoffeShop.mp4",
-        link: "https://www.google.com"
+        link: "https://github.com/noumanhafeez/Coffee_Shop"
     },
     {
         title: "Pizza App",
         description: "A React Native pizza app featuring an intuitive interface, allowing users to easily browse the menu and place orders seamlessly.",
         tags: ["React", "React Navigation", "User Experience"],
         imageUrl: "/pizza.png",
-        link: "https://www.google.com"
+        link: "https://github.com/noumanhafeez/FoodApp"
     },
     {
         title: "Login and SignUp App",
         description: "A React Native login and signup app featuring a user-friendly interface for secure account management.",
         tags: ["React", "React Navigation", "Authentication"],
         imageUrl: "/login.png",
-        link: "https://www.google.com"
+        link: "https://github.com/noumanhafeez/user_profile"
     },
     
 ]
