156eb7ef7bd4fb77e7b80d03d0b721315f9fb7c8
noumanhafeez <noumanhafeez095@gmail.com>
2025-08-09 18:11:46 +0500

    add N logo and edit navigation style

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import NextLink from "next/link";
@@ -7,58 +7,76 @@ import { Link } from "@/lib/type";
 import { useActiveSectionContext } from "@/containers/active-section";
 import { motion } from "framer-motion";
 
-
-
 type HeaderProps = { links: Link[] };
 
-export default function Header({links}: HeaderProps) {
-    const { activeSection, setActiveSection, setTimeOfLastClick} = useActiveSectionContext();
-    return (
-        <header className="hidden md:flex items-center justify-center fixed z-[999] w-full mt-4">
-            <motion.div
-            initial={{y: -100, opacity:0}}
-            animate={{ y: 0, opacity: 1}}
-            className="flex p-1 rounded-none border border-white
-            border-opacity-40 bg-white bg-opacity-80 shadow-lg
-            shadow-black/[0.3] borderop-blur-[0.5rem] sm:rounded-full
-            dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75">
-                <ul className="flex flex-wrap items-center justify-center gap-y-1 text-[0.9rem] 
-                font-medium text-gray-500">
-                    {links.map((link) => (
-                        <motion.li
-                        key={link.hash}
-                        initial={{y: -100, opacity: 0}}
-                        animate={{ y:0 , opacity:1}}
-                        className="flex items-center justify-center relative">
-                        <NextLink className={clsx(
-                            "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
-                            {
-                                "text-gray-950 dark:text-gray-200":
-                                activeSection === link.hash
-                            }
-                        )}
-                        href={link.hash}
-                        onClick={() => {
-                            setActiveSection(link.hash);
-                            setTimeOfLastClick(Date.now());
-                        }}>
-                            {link.nameEng}
-                            {link.hash === activeSection && (
-                                <motion.span
-                                transition={{
-                                    type: "spring",
-                                    stiffness: 400,
-                                    damping:30
-                                }} 
-                                layoutId="activeSection"
-                                className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                                ></motion.span>
-                            )}
-                        </NextLink>
-                    </motion.li>
-                    ))}
-                </ul>
-            </motion.div>
-        </header>
-    )
+export default function Header({ links }: HeaderProps) {
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
+
+  return (
+    <header className="fixed z-[999] w-full">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        className="flex items-center justify-between px-6
+                   border border-white border-opacity-40 
+                   bg-white bg-opacity-80 shadow-lg shadow-white/30
+                   backdrop-blur-[0.5rem] h-20
+                   dark:bg-white dark:border-white/80 dark:bg-opacity-75"
+      >
+        {/* Logo */}
+        <div
+          className="text-6xl font-extrabold 
+             bg-gradient-to-r from-gray-400 via-gray-800 to-gray-700 
+             bg-clip-text text-transparent
+             drop-shadow-[4px_4px_3px_rgba(0,0,0,0.5)]
+             -rotate-2"
+        >
+          N
+        </div>
+
+        {/* Navigation */}
+        <ul
+          className="flex flex-wrap items-center gap-x-6 text-[0.9rem] 
+                     font-medium text-gray-100"
+        >
+          {links.map((link) => (
+            <motion.li
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              className="relative"
+            >
+              <NextLink
+                className={clsx(
+                  "flex items-center px-3 py-2  transition dark:text-gray-500 dark:hover:text-yellow-200",
+                  {
+                    " dark:text-gray-200": activeSection === link.hash,
+                  }
+                )}
+                href={link.hash}
+                onClick={() => {
+                  setActiveSection(link.hash);
+                  setTimeOfLastClick(Date.now());
+                }}
+              >
+                {link.nameEng}
+                {link.hash === activeSection && (
+                  <motion.span
+                    transition={{
+                      type: "spring",
+                      stiffness: 400,
+                      damping: 30,
+                    }}
+                    layoutId="activeSection"
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                  ></motion.span>
+                )}
+              </NextLink>
+            </motion.li>
+          ))}
+        </ul>
+      </motion.div>
+    </header>
+  );
 }
