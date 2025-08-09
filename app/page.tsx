881a9deccb18477ae8acfc4c7aeabb4cfb97102b
noumanhafeez <noumanhafeez095@gmail.com>
2025-08-09 19:21:51 +0500

    move Projects above About section

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center pt-20 md:pt-28 px-4"> 
-      <Intro/>
-      <About/>
-      <Projects/>
-      <Skills/>
+    <main className="flex flex-col items-center pt-20 md:pt-28 px-4">
+      <Intro />
+      <Projects />
+      <About />
+      <Skills />
     </main>
   );
 }
