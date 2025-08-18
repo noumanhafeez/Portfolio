30fd7a59e8a258e9dc69f9db5a53f2f60f030325
noumanhafeez <noumanhafeez095@gmail.com>
2025-08-18 14:41:59 +0500

    add new descriptions and change header

--- components/about.tsx
+++ components/about.tsx
@@ -1,71 +1,91 @@
-"use client"
+"use client";
 
 import React from "react";
-import SectionHeading from "./section-heading";
 import Image from "next/image";
 import { useSectionInView } from "@/lib/useInView";
 import { motion } from "framer-motion";
 import { Fade } from "react-awesome-reveal";
 
 export default function About() {
-    const { ref } = useSectionInView("#about");
-    return (
-        <motion.section
-         id="about"
-         initial={{opacity:0, y:100}}
-         animate={{opacity:1, y:0}}
-         transition={{delay:0.175}}
-         ref={ref}
-         className="max-w-[45rem] text-center mt-32 leading-8 mb-28 sm:mb-40 scroll-mt-28">
-            <div className="container mx-auto">
-                <Fade direction="up" delay={400} cascade damping={1e-1} triggerOnce={true}>
-                <SectionHeading>
-                    about Me
-                </SectionHeading>
-                </Fade>
-                <div className="grid xl:grid-cols-2 lg:text-start">
-                    <div className="flex-1">
-                        <div className="text-lg mt-12 xl:mt-3">
-                            <div className="flex justify-start flex-col">
-                            <Fade direction="up" delay={400} cascade damping={1e-1} triggerOnce={true}>
-                                <h3 className="font-bold mt-6">
-                                    My Mission
-                                </h3>
-                                </Fade>
-                                <Fade direction="up" delay={600} cascade damping={1e-1} triggerOnce={true}>
-                                <p className="mt-2 leading-relaxed text-sm text-gray-700 dark:text-white/70">
-                                My mission is to create exceptional mobile experiences that seamlessly bridge the gap between iOS and Android platforms. I am dedicated to developing intuitive, high-performance applications that solve real-world problems and enhance user experiences through innovative React Native solutions.
-                                </p>
-                                </Fade>
-                                <Fade direction="up" delay={800} cascade damping={1e-1} triggerOnce={true}>
-                                <h3 className="font-bold mt-6">
-                                    My Vision
-                                </h3>
-                                </Fade>
-                                <Fade direction="up" delay={1000} cascade damping={1e-1} triggerOnce={true}>
-                                <p className="mt-2 leading-relaxed text-sm text-gray-700 dark:text-white/70">
-                                I envision pushing the boundaries of mobile development by crafting seamless cross-platform applications that set new standards for user interaction. My goal is to contribute to the evolution of mobile technology while delivering solutions that combine technical excellence with intuitive design.
-                                </p>
-                                </Fade>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Right Image */}
-                    <div>
-                    <Fade direction="right" delay={600} cascade damping={1e-1} triggerOnce={true}>
-                    <Image
-                    src="/about.png"
-                    alt="About Me"
-                    width="800"
-                    height="800"
-                    quality="100"
-                    priority={true}
-                    className="rounded-full mt-8 object-cover"/>
-                    </Fade>
-                    </div>
-                </div>
+  const { ref } = useSectionInView("#about");
 
-            </div>
-        </motion.section>
-    )
+  return (
+    <motion.section
+      id="about"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      ref={ref}
+      className="w-full max-w-6xl mx-auto text-center mt-32 leading-8 mb-28 sm:mb-40 scroll-mt-28 px-4"
+    >
+      <div className="container mx-auto">
+        <Fade direction="up" delay={400} cascade damping={1e-1} triggerOnce>
+          <h2 className="text-3xl font-bold text-black">About Me</h2>
+        </Fade>
+
+        <div className="grid xl:grid-cols-[1fr_2fr] lg:grid-cols-[1fr_2fr] gap-8 mt-8 items-center">
+          {/* Left Image */}
+          <div className="flex justify-center">
+            <Fade
+              direction="left"
+              delay={600}
+              cascade
+              damping={1e-1}
+              triggerOnce
+            >
+              <Image
+                src="/ghibli.png"
+                alt="About Me"
+                width="500"
+                height="200"
+                quality="100"
+                priority
+                className="rounded-3xl object-cover w-[350px] h-[480px]"
+              />
+            </Fade>
+          </div>
+
+          {/* Right Text */}
+          <div className="text-lg text-left">
+            <Fade direction="up" delay={600} cascade damping={1e-1} triggerOnce>
+              <p className="mt-2 leading-relaxed text-base text-black">
+                I’m Nouman Hafeez, a passionate and detail-oriented software
+                engineer currently pursuing a Master’s in Computer Science at{" "}
+                <strong>FAST NUCES</strong>, Lahore. I have hands-on experience
+                in machine learning, web scraping, and mobile application
+                development. Over the years, I’ve worked on diverse projects
+                involving Machine Learning, Scrapy, and React Native that are
+                not only functional but also efficient and scalable.
+              </p>
+              <p className="mt-4 leading-relaxed text-base text-black">
+                I thrive on exploring new technologies, solving complex
+                challenges, and writing clean, maintainable code. Beyond
+                academics, I’ve gained valuable industry experience through
+                internship, including working as a{" "}
+                <strong>Machine Learning Intern at Arbisoft</strong> and as a{" "}
+                <strong>Associate Software Engineer at Sortup Dev</strong>.
+                These roles have allowed me to bridge the gap between
+                data-driven intelligence and user-friendly applications. My
+                mission is to contribute to impactful, innovative projects while
+                continually growing as a developer—and to make the journey as
+                enjoyable as the destination.
+              </p>
+              {/* Mail Link */}
+              <p className="mt-6 text-base text-black">
+                Feel free to reach out to me at{" "}
+                <a
+                  href="mailto:noumanhafeez@email.com"
+                  className="text-blue-600 hover:underline"
+                >
+                  <strong>noumanhafeez@email.com</strong>
+                </a>
+              </p>
+            </Fade>
+          </div>
+        </div>
+        {/* Separator line */}
+        <hr className="mt-32 border-t-2 border-gray-300 w-1/2 mx-auto" />
+      </div>
+    </motion.section>
+  );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -7,12 +7,12 @@ import { useSectionInView } from "@/lib/useInView";
 
 export default function Intro() {
   const { ref } = useSectionInView("#home", 0.5);
-  //const { setActiveSection, setTimeOfLastClick} = useActiveSectionContext()
+
   return (
     <section
-      ref={ref}
-      className=" mb-28 max-w-[75rem] text-center sm:mb-0"
       id="home"
+      ref={ref}
+      className="mb-28 max-w-[75rem] text-center sm:mb-0 scroll-mt-20"
     >
       <div className="flex items-center justify-center">
         <div className="relative">
@@ -36,14 +36,13 @@ export default function Intro() {
         damping={1e-1}
         triggerOnce={true}
       >
-        <h1 className="mb-10 mt-4 px-4 sm:text-4xl text-black font-bold">
+        <h1 className="mb-10 mt-4 px-4 sm:text-4xl text-slate-900 font-bold">
           <span className="!leading-[1.5] relative inline-block">
-            <span className="!leading-[2.5] text-6xl pb-2 border-b-4 border-yellow-100">
+            <span className="!leading-[2.5] text-4xl pb-2 border-b-4 border-slate-300">
               Nouman Hafeez
-              <span className="absolute left-0 right-0 -bottom-2 mx-auto h-1 w-32 bg-yellow-100 rounded-full"></span>
             </span>
           </span>
-          <p className="text-[16px] font-bold text-black mt-5">
+          <p className="text-[16px] font-bold text-slate-900 mt-5">
             Hello! Glad to see you here. This is a collection of my personal
             projects created during my MS studies, combining what I learned in
             class with my own curiosity and creativity.
