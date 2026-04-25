f70338779cb1fd59eb5bbe727a2783a508a04876
noumanhafeez <noumanhafeez095@gmail.com>
2026-04-26 02:04:13 +0500

    uninstall route-start and update router code

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,7 @@
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-function DefaultErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
-  return (
-    <div>
-      <h1>Error</h1>
-      <pre>{error.message}</pre>
-      <button onClick={reset}>Try again</button>
-    </div>
-  );
-}
-
 export const router = createRouter({
   routeTree,
   context: {},
-  scrollRestoration: true,
-  defaultPreloadStaleTime: 0,
-  defaultErrorComponent: DefaultErrorComponent,
 });
